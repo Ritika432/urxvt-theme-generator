9f482117a86dd7ef14a7c1d9a4dfbd6259b639c6
Subhrajit <subhrajit1997@gmail.com>
2018-07-16 18:09:23 +0530

    initial commit

---
+++ themegen.py
@@ -0,0 +1,38 @@
+import os
+import click
+
+from blendit import blendit
+from settings import *
+
+@click.command()
+@click.argument("filename", type=click.Path(exists=True))
+@click.option("--wr", is_flag=True, help="only write to Xresources")
+@click.option("--wall", is_flag=True, help="set the file as wallpaper")
+
+def cli(filename,wr,wall):
+    """ CLI entry point """
+
+    argb, argbi = blendit.calculate(filename)
+
+    print("Average color : ", argb)
+    print("Invert of average color : ", argbi)
+
+    if wr:
+        if os.path.exists(XRS):
+            with open(XRS, "r+") as xr:
+                xr.seek(0)
+                content = xr.read().split("\n")
+                for x in content:
+                    if "*.background" in x:
+                        content[content.index(x)] = "*.background:  {}".format(argb)
+                    if "*.foreground" in x:
+                        content[content.index(x)] = "*.foreground:  {}".format(argbi)
+
+                xr.seek(0)
+                xr.write("\n".join(content))
+            os.system("xrdb {}".format(XRS))
+        else:
+            click.echo("Invalid path to Xresources file.")
+              
+    if wall:
+        os.system("feh --bg-fill {}".format(filename))
